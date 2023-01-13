34afcd02bb918112f22c4a1511002408c790ccd6
AdalidH1 <118926555+AdalidH1@users.noreply.github.com>
2023-01-13 14:37:32 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Práctica con CodeSandBox!</h1>
+<h2>ADALID HERRERA RUIZ</h2>
+<div>
+  UNIVERSIDAD TECNOLOGICA DE DURANGO
+  <a href="https://github.com/AdalidH1/contador.git" target="_blank" rel="noopener noreferrer">MI GITHUB</a>.
+</div>
+`;
